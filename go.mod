28c2419c8f38f21f91396bb3f4209bb44bb13e16
BogPin <bogdanpin2002@gmail.com>
2023-03-25 22:16:55 +0200

    create simple server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BogPin/real-time-chat
 
 go 1.17
+
+require github.com/lib/pq v1.10.7 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi user")
+}
 
 func main() {
-	fmt.Println("Hello!")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
+	host := "localhost"
+	db, err := dbInit(user, password, host, dbname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func dbInit(user, password, host, dbname string) (*sql.DB, error) {
+	conStr := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable", user, password, host, dbname)
+	return sql.Open("postgres", conStr)
 }
