c8de49abfa7d2cfe27bedf4bfaa69129ef351e16
BogPin <bogdanpin2002@gmail.com>
2023-04-08 22:32:47 +0300

    add entity chat

---
+++ controllers/chats.go
@@ -0,0 +1,105 @@
+package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"github.com/BogPin/real-time-chat/models/chat"
+	"github.com/BogPin/real-time-chat/services"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+func RegisterChatsRoutes(router *mux.Router, service services.Chat) {
+	router.Path("/").HandlerFunc(createChat(service)).Methods("POST")
+	router.Path("/").HandlerFunc(updateChat(service)).Methods("PATCH")
+	router.Path("/{id}").HandlerFunc(getChat(service)).Methods("GET")
+	router.Path("/{id}").HandlerFunc(deleteChat(service)).Methods("DELETE")
+}
+
+func getChat(service services.Chat) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		chat, err := service.GetChat(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = json.NewEncoder(w).Encode(chat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func deleteChat(service services.Chat) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		chat, err := service.Delete(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = json.NewEncoder(w).Encode(chat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func updateChat(service services.Chat) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var chat chat.Chat
+		err := json.NewDecoder(r.Body).Decode(&chat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		newChat, err := service.Update(chat)
+
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, err.Error(), http.StatusNotFound)
+				return
+			}
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = json.NewEncoder(w).Encode(newChat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func createChat(service services.Chat) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var chatDTO chat.ChatDTO
+		err := json.NewDecoder(r.Body).Decode(&chatDTO)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		chat, err := service.Create(chatDTO)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		err = json.NewEncoder(w).Encode(chat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
---
+++ services/chats.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"github.com/BogPin/real-time-chat/models/chat"
+)
+
+type Chat interface {
+	GetAllChats() ([]chat.Chat, error)
+	GetChat(id int) (*chat.Chat, error)
+	Update(chat chat.Chat) (*chat.Chat, error)
+	Delete(id int) (*chat.Chat, error)
+	Create(ChatTDO chat.ChatDTO) (*chat.Chat, error)
+}
+
+type ChatService struct {
+	ChatStorer *chat.ChatStorer
+}
+
+func (cs *ChatService) GetAllChats() ([]chat.Chat, error) {
+	return cs.ChatStorer.GetAllChats()
+}
+
+func (cs *ChatService) GetChat(id int) (*chat.Chat, error) {
+	return cs.ChatStorer.GetChat(id)
+}
+
+func (cs *ChatService) Update(chat chat.Chat) (*chat.Chat, error) {
+	return cs.ChatStorer.Update(chat)
+}
+
+func (cs *ChatService) Create(chatDTO chat.ChatDTO) (*chat.Chat, error) {
+	return cs.ChatStorer.Create(chatDTO)
+}
+
+func (cs *ChatService) Delete(id int) (*chat.Chat, error) {
+	return cs.ChatStorer.Delete(id)
+}
